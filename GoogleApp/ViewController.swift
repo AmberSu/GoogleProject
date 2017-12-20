23e3b52fb581b911207a2745027efb2138d0f6c7
MacOS <MacOS@MacOSs-MacBook-Air.local>
2017-12-20 20:24:41 +0200

    initial commit

--- GoogleApp/ViewController.swift
+++ GoogleApp/ViewController.swift
@@ -8,18 +8,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var resultTitles = [String]()
+    
+    @IBAction func searchInGoogle(_ sender: UIButton) {
+        resultTitles.removeAll()
+        sendRequest()
+    }
+    
+    @IBAction func showResults(_ sender: UIButton) {
+        tableView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func sendRequest() {
+        let configuration = URLSessionConfiguration.default
+        let session = URLSession(configuration: configuration, delegate: nil, delegateQueue: OperationQueue.main)
+        let searchWords: String = String(textField.text!)
+        let words = searchWords.split(separator: " ")
+        for word in words {
+            guard let url = URL(string: "https://www.googleapis.com/customsearch/v1?key=AIzaSyCRx1TzMXvZmNn7hllGx_VPMFHFp7vvVD8&cx=015479572664441330661:fcm_kei-4um&q=" + String(word)) else {
+                return
+            }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "GET"
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        let task = session.dataTask(with: urlRequest, completionHandler: { [weak self] (data: Data?, response: URLResponse?, error: Error?) -> Void in
+            if error != nil {
+                print(error as Any)
+            }
+            guard let data = data else {
+                return
+            }
+            guard let json = try?
+                JSONSerialization.jsonObject(with: data, options: .mutableContainers) else {
+                return
+            }
+            
+            guard let results = json as? Dictionary<String, Any>, let items = results["items"] as? Array<Dictionary<String, Any>> else {
+                return
+            }
+            for item in items {
+                let htmlTitle = item["title"] as? String
+                self?.resultTitles.append(htmlTitle!)
+            }
+            print(self?.resultTitles ?? "Title")
+        })
+        task.resume()
+        }
     }
-
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return resultTitles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tableViewCell: UITableViewCell = UITableViewCell(style: .subtitle, reuseIdentifier: "Cell")
+        tableViewCell.textLabel?.numberOfLines = 2
+        let titles = resultTitles[indexPath.row]
+        tableViewCell.textLabel?.text = titles
+        return tableViewCell
+    }
 }
 
